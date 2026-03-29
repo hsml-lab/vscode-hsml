eab30afc859f699d20b651b566333b2c642f3d7b
Shinigami92 <chrissi92@hotmail.de>
2026-03-29 20:59:10 +0200

    feat: improve TextMate grammar

--- tests/grammar.test.ts
+++ tests/grammar.test.ts
@@ -73,22 +73,36 @@ describe('comments', () => {
   });
 });
 
+describe('doctype', () => {
+  it('should tokenize doctype keyword', () => {
+    const tokens = tokenize('doctype html');
+    expect(findToken(tokens, 'keyword.other.doctype.hsml')?.text).toBe('doctype');
+  });
+
+  it('should tokenize doctype type', () => {
+    const tokens = tokenize('doctype html');
+    expect(findToken(tokens, 'entity.name.type.hsml')?.text).toBe('html');
+  });
+});
+
 describe('tag names', () => {
   it('should tokenize a tag followed by space', () => {
     const tokens = tokenize('div Hello');
-    expect(findToken(tokens, 'entity.name.tag.hsml')).toBeDefined();
     expect(findToken(tokens, 'entity.name.tag.hsml')?.text).toBe('div');
   });
 
   it('should tokenize a tag followed by dot', () => {
     const tokens = tokenize('div.container ');
-    expect(findToken(tokens, 'entity.name.tag.hsml')).toBeDefined();
+    expect(findToken(tokens, 'entity.name.tag.hsml')?.text).toBe('div');
+  });
+
+  it('should tokenize a tag followed by hash', () => {
+    const tokens = tokenize('div#myid ');
     expect(findToken(tokens, 'entity.name.tag.hsml')?.text).toBe('div');
   });
 
   it('should tokenize a tag followed by parenthesis', () => {
     const tokens = tokenize('img(src="photo.jpg")');
-    expect(findToken(tokens, 'entity.name.tag.hsml')).toBeDefined();
     expect(findToken(tokens, 'entity.name.tag.hsml')?.text).toBe('img');
   });
 
@@ -96,12 +110,40 @@ describe('tag names', () => {
     const tokens = tokenize('h1 Title');
     expect(findToken(tokens, 'entity.name.tag.hsml')?.text).toBe('h1');
   });
+
+  it('should tokenize custom elements with hyphens', () => {
+    const tokens = tokenize('my-component Hello');
+    expect(findToken(tokens, 'entity.name.tag.hsml')?.text).toBe('my-component');
+  });
+});
+
+describe('IDs', () => {
+  it('should tokenize an ID selector', () => {
+    const tokens = tokenize('div#myid ');
+    expect(findToken(tokens, 'entity.other.attribute-name.id.css.hsml')?.text).toBe('myid');
+  });
+
+  it('should tokenize the hash separator', () => {
+    const tokens = tokenize('div#myid ');
+    expect(findToken(tokens, 'meta.attribute-name.id.css.hsml')?.text).toBe('#');
+  });
+
+  it('should tokenize IDs with hyphens', () => {
+    const tokens = tokenize('div#my-id ');
+    expect(findToken(tokens, 'entity.other.attribute-name.id.css.hsml')?.text).toBe('my-id');
+  });
+
+  it('should tokenize tag with ID and class', () => {
+    const tokens = tokenize('div#myid.myclass ');
+    expect(findToken(tokens, 'entity.name.tag.hsml')?.text).toBe('div');
+    expect(findToken(tokens, 'entity.other.attribute-name.id.css.hsml')?.text).toBe('myid');
+    expect(findToken(tokens, 'entity.other.attribute-name.class.css.hsml')?.text).toBe('myclass');
+  });
 });
 
 describe('classes', () => {
   it('should tokenize a class name', () => {
     const tokens = tokenize('div.container ');
-    expect(findToken(tokens, 'entity.other.attribute-name.class.css.hsml')).toBeDefined();
     expect(findToken(tokens, 'entity.other.attribute-name.class.css.hsml')?.text).toBe('container');
   });
 
@@ -127,23 +169,63 @@ describe('classes', () => {
     const tokens = tokenize('div.bg-[#1da1f2] ');
     expect(findToken(tokens, 'entity.other.attribute-name.class.css.hsml')).toBeDefined();
   });
+
+  it('should tokenize implicit div with class', () => {
+    const tokens = tokenize('.container ');
+    expect(findToken(tokens, 'entity.other.attribute-name.class.css.hsml')?.text).toBe('container');
+  });
 });
 
 describe('attributes', () => {
-  it('should tokenize attribute name', () => {
-    const tokens = tokenize('src="photo.jpg"');
-    expect(findToken(tokens, 'entity.other.attribute-name.hsml')).toBeDefined();
+  it('should tokenize attribute name inside parens', () => {
+    const tokens = tokenize('img(src="photo.jpg")');
     expect(findToken(tokens, 'entity.other.attribute-name.hsml')?.text).toBe('src');
   });
 
-  it('should tokenize attribute value', () => {
-    const tokens = tokenize('src="photo.jpg"');
-    expect(findToken(tokens, 'string.quoted.double.hsml')).toBeDefined();
-    expect(findToken(tokens, 'string.quoted.double.hsml')?.text).toBe('"photo.jpg"');
+  it('should tokenize attribute value inside parens', () => {
+    const tokens = tokenize('img(src="photo.jpg")');
+    expect(findToken(tokens, 'string.quoted.hsml')?.text).toBe('"photo.jpg"');
   });
 
   it('should tokenize hyphenated attribute names', () => {
-    const tokens = tokenize('data-id="123"');
+    const tokens = tokenize('div(data-id="123")');
     expect(findToken(tokens, 'entity.other.attribute-name.hsml')?.text).toBe('data-id');
+  });
+
+  it('should tokenize single-quoted attribute values', () => {
+    const tokens = tokenize("div(title='hello')");
+    expect(findToken(tokens, 'string.quoted.hsml')?.text).toBe("'hello'");
+  });
+
+  it('should tokenize Vue binding attributes', () => {
+    const tokens = tokenize('img(:src="imageUrl")');
+    expect(findToken(tokens, 'entity.other.attribute-name.hsml')?.text).toBe(':src');
+  });
+
+  it('should tokenize Vue event attributes', () => {
+    const tokens = tokenize('button(@click="handler")');
+    expect(findToken(tokens, 'entity.other.attribute-name.hsml')?.text).toBe('@click');
+  });
+
+  it('should tokenize Vue directives', () => {
+    const tokens = tokenize('div(v-if="show")');
+    expect(findToken(tokens, 'entity.other.attribute-name.hsml')?.text).toBe('v-if');
+  });
+
+  it('should tokenize opening paren', () => {
+    const tokens = tokenize('div(class="foo")');
+    expect(findToken(tokens, 'punctuation.section.attributes.begin.hsml')?.text).toBe('(');
+  });
+
+  it('should tokenize closing paren', () => {
+    const tokens = tokenize('div(class="foo")');
+    expect(findToken(tokens, 'punctuation.section.attributes.end.hsml')?.text).toBe(')');
+  });
+});
+
+describe('text block', () => {
+  it('should tokenize text block marker', () => {
+    const tokens = tokenize('p.text-lg.');
+    expect(findToken(tokens, 'punctuation.definition.text-block.hsml')?.text).toBe('.');
   });
 });
